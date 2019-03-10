638392e789a7c3abd41938702d35b0ed7fa5d742
kter <me@kter.jp>
2019-03-10 10:41:34 +0000

    表示件数を制限

--- blog/updater.py
+++ blog/updater.py
@@ -5,11 +5,15 @@ from jinja2 import Environment, FileSystemLoader
 import boto3
 
 BLOG_URL = os.environ.get('BLOG_URL', 'https://blog.kter.jp/feed.xml')
+DISPLAY_LIMIT = 5
+
 d = feedparser.parse(BLOG_URL)
 TAG_RE = re.compile(r'<[^>]+>')
 
 def remove_tags(text):
     return TAG_RE.sub('', text)
+
+del d['entries'][DISPLAY_LIMIT:]
  
 for i, entry in enumerate(d['entries']):
     d['entries'][i]['remove_tags_content'] = remove_tags(entry.content[0].value)[:30]
