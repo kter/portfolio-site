e82e671f7a785c6e30f304b32f90f1a05ea4eb0a
kter <me@kter.jp>
2019-03-10 05:26:16 +0000

    Unicode Error解決

--- github/updater.py
+++ github/updater.py
@@ -9,14 +9,14 @@ r = requests.get(GITHUB_URL)
 content = r.json()
 
 for i, entry in enumerate(content):
-   content[i]['short_description'] = entry['description'].encode('utf-8')[:30] if entry['description'] else 'No Description Provided'
+   content[i]['short_description'] = entry['description'][:30] if entry['description'] else 'No Description Provided'
 
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('_github.html.py')
 output_from_parsed_template = template.render(entries=content)
 
-with open("_github.html", "wb") as fh:
-    fh.write(output_from_parsed_template)
+with open("_github.html", "w") as fh:
+    fh.write(output_from_parsed_template.encode('utf-8'))
 
 BUCKET_NAME = os.environ.get('BUCKET_NAME', 'tomohiko.io')
 s3 = boto3.resource('s3')
