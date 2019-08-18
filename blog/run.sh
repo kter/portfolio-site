7583fc42080cc1f8f3fadad576b212b0ad0049ce
Takahashi Tomohiko <takahashi@tomohiko.io>
2019-08-18 15:37:13 +0900

    js化したので削除

--- blog/run.sh
+++
@@ -1,4 +0,0 @@
-docker build -t portfolio-site/blog_updater .
-docker tag portfolio-site/blog_updater:latest 848738341109.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-site/blog_updater:latest
-docker push 848738341109.dkr.ecr.ap-northeast-1.amazonaws.com/portfolio-site/blog_updater:latest
-docker run -e BLOG_URL=https://kter.jp/feed.xml -e BUCKET_NAME=kter.jp portfolio-site/blog_updater
